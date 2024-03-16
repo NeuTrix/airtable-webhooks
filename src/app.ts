ab377501eaea867be876553079d48d1fdcfc5248
Michael J. Walker <neutrix@brandgeneering.com>
2024-03-16 10:25:44 -0500

    add hello world home page

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import peopleRoutes from './routes/people';
 const app = express();
 
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.use('/people', peopleRoutes);
 
 export default app;
