9272e371e9466ef477e4ad2b2cd5ae2f11c592df
Michael J. Walker <neutrix@brandgeneering.com>
2024-03-16 11:20:42 -0500

    try require for dotenv

--- dist/server.js
+++ dist/server.js
@@ -3,8 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 const app_1 = __importDefault(require("./app"));
 const port = process.env.PORT || 3000;
 app_1.default.listen(port, () => {
