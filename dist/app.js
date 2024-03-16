7daad7a58f33a605da0c22d2808a60edcd8a3612
Michael J. Walker <neutrix@brandgeneering.com>
2024-03-16 10:35:49 -0500

    remove postbuild

--- dist/app.js
+++ dist/app.js
@@ -8,5 +8,8 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const people_1 = __importDefault(require("./routes/people"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 app.use('/people', people_1.default);
 exports.default = app;
